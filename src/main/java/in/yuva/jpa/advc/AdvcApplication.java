a7e425ab171f01152ec05d39d64769a7555e20ab
Yuvaraj <nyuvaraj24@gmail.com>
2025-09-20 19:55:07 +0530

    initial commit

---
+++ src/main/java/in/yuva/jpa/advc/AdvcApplication.java
@@ -0,0 +1,13 @@
+package in.yuva.jpa.advc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvcApplication.class, args);
+	}
+
+}
